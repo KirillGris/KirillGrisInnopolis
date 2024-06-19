546c45ed884d187cac4e3b9c7ee3fba76f59cf4e
KirillGrisInnopolis <mr.Gk-7@yandex.ru>
2024-06-19 21:52:15 +0300

    Коммит глобального gitignore

---
+++ firstHomework/src/Homework4/SortWords.java
@@ -0,0 +1,35 @@
+package Homework4;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку, состоящую из двух слов, разделенных пробелом:");
+        String input = scanner.nextLine();
+
+        // Разделяем строку на два слова
+        String[] words = input.split(" ");
+
+        if (words.length != 2) {
+            System.out.println("Ошибка: Превышено количество слов");
+            return;
+        }
+
+        // Преобразуем каждое слово в нижний регистр и сортируем по символам
+        String sortedWord1 = sortWord(words[0].toLowerCase());
+        String sortedWord2 = sortWord(words[1].toLowerCase());
+
+        // Выводим результат
+        System.out.println("Упорядоченные слова: " + sortedWord1 + " " + sortedWord2);
+    }
+
+    private static String sortWord(String word) {
+        // Преобразуем слово в массив символов
+        char[] chars = word.toCharArray();
+        // Сортируем массив символов
+        Arrays.sort(chars);
+        // Преобразуем отсортированный массив обратно в строку
+        return new String(chars);
+    }
+}
